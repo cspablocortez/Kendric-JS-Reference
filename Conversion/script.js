d5d117848bc4622bf33610341f0f74e0099ce4db
Pablo Cortez <cspablocortez@gmail.com>
2024-01-11 18:08:59 -0800

    Update Conversion tool

--- Conversion/script.js
+++ Conversion/script.js
@@ -1,5 +1,8 @@
-console.log("Hello from the script!");
+function convert(e) {
+    const elemID = e.id;
 
-function test() {
-    
+    console.log(`Hello from ${elemID}`);
+
+    // 1. Figure out if we are working with Fº or Cº
+
 }
