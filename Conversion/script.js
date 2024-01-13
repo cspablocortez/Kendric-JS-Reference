08b3f6ca0b15f20460ff6d4c7f631991b768b53a
Pablo Cortez <cspablocortez@gmail.com>
2024-01-12 16:32:44 -0800

    Update script.js

--- Conversion/script.js
+++ Conversion/script.js
@@ -1,8 +1,18 @@
 function convert(e) {
     const elemID = e.id;
+    let degrees = document.getElementById(elemID).value;
+    console.log(`${degrees} input from ${elemID}`);
+    elemID == 'fahrInput' ? getCelsius(degrees) : getFahr(degrees);
+    // console.log(result);
+}
 
-    console.log(`Hello from ${elemID}`);
+function getCelsius(d) {
+    let f = parseFloat(d);
+    let result = (5/9) * (f - 32);
+    document.getElementById('celsiusInput').textContent = result;
+}
 
-    // 1. Figure out if we are working with Fº or Cº
-
+function getFahr(d) {
+    let c = parseFloat(d);
+    c = (5/9) * (f - 32);
 }
